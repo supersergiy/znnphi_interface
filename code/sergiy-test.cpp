330a94a1754f9fc28375b3339ae6631e238eac33
Sergiy Popovych <popovych@seungworkstation2002.Princeton.EDU>
2017-05-01 13:23:41 -0400

    testbed

--- code/sergiy-test.cpp
+++ code/sergiy-test.cpp
@@ -4,7 +4,7 @@ using namespace znn::phi;
 
 int main()
 {
-    benchmark_forward<1, 72, 56, 3, 24, 1, 3>("conv1");
+    benchmark_forward<32, 32, 32, 18, 192, 1, 3>("conv1");
     //benchmark_forward<8, 80, 80, 300, 300, 3, 3>("conv2");
     //benchmark_forward<64, 80, 80, 192, 192, 3, 3>("conv3");
     //benchmark_forward<1, 64, 64, 570, 570, 9, 1>("conv4");

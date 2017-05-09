254b8a0f411d4c6cf1f8fb01c363bf6598bb483f
Ubuntu <ubuntu@ip-172-31-18-223.us-west-2.compute.internal>
2017-05-09 13:13:09 +0000

    different lib destinations

---
+++ code/include/znn/interface/test_client.cpp
@@ -0,0 +1,11 @@
+#include "ZnnPhiConvLayer.hpp"
+#include <iostream>
+#include <omp.h>
+#include <vector>
+
+int main()
+{
+	znn::phi::ZnnPhiConvLayer znnphi_convs;
+	std::cout << "let's go" << std::endl;
+}
+

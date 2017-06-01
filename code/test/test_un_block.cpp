5c28348081b2acdaf09c831fe6fef7614022a507
supersergiy <sergiy.popovich@gmail.com>
2017-06-01 05:35:44 -0400

    start hd5test

--- code/test/test_un_block.cpp
+++ code/test/test_un_block.cpp
@@ -21,24 +21,12 @@ void testCancellation(size_t b, size_t fm, size_t d, size_t hw)
     std::cout << "SIMD_WIDTH: " << SIMD_WIDTH << std::endl;
     bl.execute(in.data(), out1.data());
     ubl.execute(out1.data(), out2.data());
-    for (int i = 0; i < SIZE; i++) {
-        std::cout << in.data()[i] << " ";
-    }
-    std::cout << std::endl;
-    for (int i = 0; i < SIZE; i++) {
-        std::cout << out1.data()[i] << " ";
-    }
-    std::cout << std::endl;
-    for (int i = 0; i < SIZE; i++) {
-        std::cout << out2.data()[i] << " ";
-    }
-    std::cout << std::endl;
     assert(memcmp(in.data(), out2.data(), SIZE*sizeof(float)));
 }
 
 int main(void)
 {
-    testCancellation(1,16,1,2);
+    testCancellation(7,16,3,5);
 }
 
 

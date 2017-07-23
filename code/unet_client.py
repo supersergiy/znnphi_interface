7914c5c48e642029ed2693111251fbb5f8fdabe0
supersergiy <ubuntu@ip-172-31-53-83.ec2.internal>
2017-07-23 22:06:10 +0000

    m

--- code/unet_client.py
+++ code/unet_client.py
@@ -12,7 +12,7 @@ net_file = 'unet/unet.prototxt'
 
 weights_file = 'unet/unet.h5'
 
-input_file = 'data/inputs/input.h5'
+input_file = 'unet/inputs/input.h5'
 
 reference_file = 'data/reference/reference.h5'
 
@@ -34,27 +34,34 @@ for i in range(2):
     reference_file = h5py.File(reference_path)
     reference_a = reference_file["output"][:]
     np.set_printoptions(precision=2)
-    ref_a = reference_a
 
-    diff_a = reference_a - out_a
+    diff_a = reference_a - out_a                                                                                                                           
     rel_d = np.abs(diff_a) / (out_a + 0.0000000001)
     mask1 = diff_a > 1e-5
     mask2 = rel_d > 1e-5
-
-    errors = rel_d * mask1 * mask2 * reference_a 
-    error = np.sum(errors)
-
+ 
     fd = diff_a.flatten()
     fo = out_a.flatten()
     fr = reference_a.flatten()
 
+    errors = rel_d * mask1 * mask2 * reference_a
+    error = np.sum((errors*10)**2)
+
+    max_d = np.max(np.abs(diff_a))
+    i = np.argmax(np.abs(diff_a.flatten()))
+    max_rel_d = max_d / fr[i]
+    print "Max rel d: {}".format(max_rel_d)
+    print "Max d: {}".format(max_d)
+    print "Average d: {}".format(np.average(rel_d))
+
     if np.isnan(error):
         print "Not congrats! Error == {}".format(error)
+        import pdb; pdb.set_trace()
     elif error > 0.010:
         print "Not congrats! Error == {}".format(error)
+        import pdb; pdb.set_trace()
     else:
         print "Congrats! All pass. Error == {}".format(error)
-    import pdb; pdb.set_trace()
 #out_file = h5py.File(output_path)
 #out_file.create_dataset("data", data=out_a)
 

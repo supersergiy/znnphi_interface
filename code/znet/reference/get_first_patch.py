8c1d008c3e9ec0678d735e6c76438e931fbcc774
jingpengwu <jingpeng.wu@gmail.com>
2017-08-28 18:45:09 +0000

    new testt structure

--- code/znet/reference/get_first_patch.py
+++ code/znet/reference/get_first_patch.py
@@ -7,7 +7,7 @@ out_path     = "/sergiy_shared/znnphi_interface/code/znet/reference/patch.h5"
 
 in_data = h5py.File(in_path)["main"][...]
 
-out_data = in_data[0:18, 0:192, 0:192] 
+out_data = in_data[0:18, 0:192, 0:192] / 256.0
 
 out_file = h5py.File(out_path)
 out_file.create_dataset('/main', data=out_data)

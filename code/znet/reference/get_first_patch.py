6f6ad082d942cc4bb13dcb1b0badb80291a6e810
jingpengwu <jingpeng.wu@gmail.com>
2017-08-28 17:43:21 +0000

    new reference generators

---
+++ code/znet/reference/get_first_patch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import h5py
+import numpy as np
+
+in_path      = "/sergiy_shared/overlap/img.h5"
+out_path     = "/sergiy_shared/znnphi_interface/code/znet/reference/patch.h5"
+
+in_data = h5py.File(in_path)["main"][...]
+
+out_data = in_data[0:18, 0:192, 0:192] 
+
+out_file = h5py.File(out_path)
+out_file.create_dataset('/main', data=out_data)
+out_file.close()
---
+++ code/znet/reference/unet_gpu_run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import caffe
+import h5py
+import numpy as np
+
+caffe.set_device(1)
+caffe.set_mode_gpu()
+
+#model_path   = "/sergiy_shared/overlap/deploy.prototxt"
+#weights_path = "/sergiy_shared/overlap/train_iter_1000000.caffemodel.h5"
+model_path   = "/sergiy_shared/overlap/deploy.prototxt" 
+weights_path = "/sergiy_shared/overlap/train_iter_1000000.caffemodel.h5" 
+in_path      = "/sergiy_shared/znnphi_interface/code/znet/reference/patch1.h5"
+out_path     = "/sergiy_shared/znnphi_interface/code/znet/reference/unet_out.h5"
+
+
+net = caffe.Net(model_path, weights_path, caffe.TEST)
+
+in_data = h5py.File(in_path)["main"][:] / 256.0
+net.blobs["input"].data[:] = in_data
+net.forward()
+out_data = net.blobs["affinity"].data[...]
+import pdb; pdb.set_trace()
+out_file = h5py.File(out_path)
+out_file.create_dataset('/main', data=out_data)
+out_file.close()

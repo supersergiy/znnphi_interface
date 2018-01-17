28de9e389b4f75bb79d4ea9c525d65633fd79872
root <popovych@princeton.edu>
2018-01-17 17:58:44 +0000

    cleanup the tmp folder after compilation

--- code/znet/src/python/pznet/pznet.py
+++ code/znet/src/python/pznet/pznet.py
@@ -16,15 +16,15 @@ class znet:
     def __init__(self):
         self.net = None
         my_path = os.path.dirname(os.path.abspath(__file__))
-        self.real_secret_path = os.path.join(my_path, ".tmp/")
+        self.tmp_folder_path = os.path.join(my_path, ".tmp/")
 
-        if not os.path.exists(self.real_secret_path):
-            os.makedirs(self.real_secret_path)
+        if not os.path.exists(self.tmp_folder_path):
+            os.makedirs(self.tmp_folder_path)
 
-        sys.path.append(self.real_secret_path)
+        sys.path.append(self.tmp_folder_path)
 
     def create_net(self, prototxt_path, h5_weights_path, output_path, architecture='AVX2', cores=2, ht=2, cpu_offset=0):
-        json_net_path = os.path.join(self.real_secret_path, 'net.json')
+        json_net_path = os.path.join(self.tmp_folder_path, 'net.json')
         convert_prototxt_to_json(prototxt_path, json_net_path)
 
         znnphi_path = os.environ["ZNNPHI_PATH"]
@@ -33,14 +33,15 @@ class znet:
         make_command  = 'make -C {} py N={} W={} O={} ARCH={} CORES={} HT={} CPU_OFFSET={}'.format(mothership_folder,
                                                              json_net_path,
                                                              h5_weights_path,
-                                                             self.real_secret_path,
+                                                             self.tmp_folder_path,
                                                              architecture, cores, ht, cpu_offset)
         os.system(make_command) #compiles the znet.so and copies it to the working folder along with the weights
 
         #copy results to the output folder
         if not os.path.exists(output_path):
             os.makedirs(output_path)
-        os.system("cp -r {}/* {}".format(self.real_secret_path, output_path))
+        os.system("cp -r  {}/* {}".format(self.tmp_folder_path, output_path))
+        os.system("rm -rf {}/*   ".format(self.tmp_folder_path)
         os.system("cp {} {}/net.prototxt".format(prototxt_path, output_path))
         os.system("cp {} {}/weights.h5".format(h5_weights_path, output_path))
 
@@ -50,14 +51,14 @@ class znet:
         if not os.path.exists(lib_path):
             os.makedirs(lib_path)
         target_files = os.path.join(net_path, '*')
-        os.system("cp -r {} {}".format(target_files, self.real_secret_path))
+        os.system("cp -r {} {}".format(target_files, self.tmp_folder_path))
         try:
             import znet
         except:
             raise Exception("Problem loading the network object. " +
                             "Please make sure there's a znet.so file present at {}".format(net_path))
 
-        self.net = znet.znet(os.path.join(self.real_secret_path, "weights/"), lib_path)
+        self.net = znet.znet(os.path.join(self.tmp_folder_path, "weights/"), lib_path)
 
     def get_in_shape(self):
         ret = self.net.get_in_shape()

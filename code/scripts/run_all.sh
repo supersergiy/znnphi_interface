ec4c8b88c86290894b55e200cb5fe11b7398a36d
root <popovych@princeton.edu>
2018-03-28 02:26:12 +0000

    m

--- code/scripts/run_all.sh
+++ code/scripts/run_all.sh
@@ -2,7 +2,47 @@
 #git checkout benchmarking
 #./run_single_test.py /tests/unet_paper full_opt
 #./run_single_test.py /tests/unet_paper_bn full_opt
+./run_single_test.py /tests/unet_paper_elu full_opt
+./run_single_test.py /tests/unet_paper_bn_elu full_opt
+
+./run_single_test.py /tests/unet_paper_bn no_opt
+./run_single_test.py /tests/unet_paper_bn no_add
+./run_single_test.py /tests/unet_paper_bn no_act
+./run_single_test.py /tests/unet_paper_bn no_pad
+
+./run_single_test.py /tests/unet_paper_bn_elu no_opt
+./run_single_test.py /tests/unet_paper_bn_elu no_add
+./run_single_test.py /tests/unet_paper_bn_elu no_act
+./run_single_test.py /tests/unet_paper_bn_elu no_pad
+
+./run_single_test.py /tests/unet_sym full_opt
+./run_single_test.py /tests/unet_sym_bn full_opt
+./run_single_test.py /tests/unet_sym_elu full_opt
+./run_single_test.py /tests/unet_sym_bn_elu full_opt
+
+./run_single_test.py /tests/unet_sym_bn no_opt
+./run_single_test.py /tests/unet_sym_bn no_add
+./run_single_test.py /tests/unet_sym_bn no_act
+./run_single_test.py /tests/unet_sym_bn no_pad
+
+./run_single_test.py /tests/unet_sym_bn_elu no_opt
+./run_single_test.py /tests/unet_sym_bn_elu no_add
+./run_single_test.py /tests/unet_sym_bn_elu no_act
+./run_single_test.py /tests/unet_sym_bn_elu no_pad
+
 git checkout benchmarking_pni
 #./run_single_test.py /tests/pni_unet full_opt
+./run_single_test.py /tests/pni_unet_no_bn full_opt
+./run_single_test.py /tests/pni_unet_relu full_opt
+./run_single_test.py /tests/pni_unet_no_bn_relu full_opt
+
 #./run_single_test.py /tests/pni_unet no_opt
-#git checkout benchmarking
+./run_single_test.py /tests/pni_unet no_add
+./run_single_test.py /tests/pni_unet no_act
+./run_single_test.py /tests/pni_unet no_pad
+
+./run_single_test.py /tests/pni_unet_relu no_opt
+./run_single_test.py /tests/pni_unet_relu no_add
+./run_single_test.py /tests/pni_unet_relu no_act
+./run_single_test.py /tests/pni_unet_relu no_pad
+git checkout benchmarking

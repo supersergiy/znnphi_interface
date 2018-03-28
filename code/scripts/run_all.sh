da214bc3ecab5b0128498eb0660665ea756d1e1e
root <popovych@princeton.edu>
2018-03-28 01:42:47 +0000

    m

--- code/scripts/run_all.sh
+++ code/scripts/run_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-./run_single_test.py /tests/unet_paper full_opt
-./run_single_test.py /tests/unet_paper_bn full_opt
-git checkout bench_pni
+#git checkout benchmarking
+#./run_single_test.py /tests/unet_paper full_opt
+#./run_single_test.py /tests/unet_paper_bn full_opt
+git checkout benchmarking_pni
 ./run_single_test.py /tests/pni_unet full_opt
-./run_single_test.py /tests/pni_unet_relu full_opt
+./run_single_test.py /tests/pni_unet no_opt
+git checkout benchmarking

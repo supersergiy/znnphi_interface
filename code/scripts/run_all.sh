2662f4c4c9878caaacb723abe9cb729527f66c03
root <popovych@princeton.edu>
2018-03-28 01:47:25 +0000

    m

--- code/scripts/run_all.sh
+++ code/scripts/run_all.sh
@@ -3,6 +3,6 @@
 #./run_single_test.py /tests/unet_paper full_opt
 #./run_single_test.py /tests/unet_paper_bn full_opt
 git checkout benchmarking_pni
-./run_single_test.py /tests/pni_unet full_opt
-./run_single_test.py /tests/pni_unet no_opt
-git checkout benchmarking
+#./run_single_test.py /tests/pni_unet full_opt
+#./run_single_test.py /tests/pni_unet no_opt
+#git checkout benchmarking

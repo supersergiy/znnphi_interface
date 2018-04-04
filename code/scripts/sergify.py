56031c02addcf38916f92256c18e146b08c1efc5
root <popovych@princeton.edu>
2018-04-04 19:06:26 +0000

    modify sergify

--- code/scripts/sergify.py
+++ code/scripts/sergify.py
@@ -15,12 +15,12 @@ parser.add_option("-w", "--weights", dest="weights_path",
 parser.add_option("-t", "--timelayers",
                           action="store_false", dest="time_layers", default=False,
                           help="Time each layer separately")
-parser.add_option("-c", "--cores",
-                          dest="n_cores", default=2,
-                          help="Number of cores to use")
-parser.add_option("--ht",
-                          dest="n_ht", default=2,
-                          help="Number of hyperthread per core")
+parser.add_option("-c", "--cores", dest="conv_cores", default=2)
+parser.add_option("--ht", dest="conv_ht", default=2)
+parser.add_option("--act_cores", dest="act_cores", default=-1)
+parser.add_option("--act_ht", dest="act_ht", default=-1)
+parser.add_option("--lin_cores", dest="lin_cores", default=-1)
+parser.add_option("--lin_ht", dest="lin_ht", default=-1)
 parser.add_option("-o", dest="output_path", default="./generated_netwrok",
                         help="The output folder for the generated network")
 
@@ -36,13 +36,16 @@ if not options.weights_path:
 weights_path = options.weights_path
 net_path  = options.prototxt_path
 znet_path = os.path.abspath(options.output_path)
-n_cores   = options.n_cores
-n_ht      = options.n_ht
 arch	  = options.architecture
+
+core_options = {}
+core_options["conv"] = [options.conv_cores, options.conv_ht]
+core_options["act"]  = [options.act_cores, options.act_ht]
+core_options["lin"]  = [options.lin_cores, options.lin_ht]
 
 z = pznet.znet()
 print "Creating the network..."
-z.create_net(net_path, weights_path, znet_path, arch, n_cores, n_ht)
+z.create_net(net_path, weights_path, znet_path, arch, core_options=core_options)
 print "Compiling layers..."
 z.load_net(znet_path, os.path.join(znet_path, "lib"))
 print "Your network has been sergified! You can find it at {}".format(znet_path)
